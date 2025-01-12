5e6c1b1d850b09c5da040f0c4fec00aea614957e
syn <postbox.501@yandex.com>
2025-01-12 20:27:45 +0530

    added signin page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import Base from './components/Base';
 import Homepage from './pages/Homepage';
 import Error_page from './pages/Error_page';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Base />}>
               <Route path="/" element={<Homepage />} />
+              <Route path='sign_in/' element={<SignIn />} />
+
+              {/* Error page */}
               <Route path='*' element={<Error_page />} />
             </Route>
           </Routes>
