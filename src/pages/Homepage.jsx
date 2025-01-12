486aee38e1909bddb5e2883f2978ee956d15b57f
syn <postbox.501@yandex.com>
2025-01-12 14:13:55 +0530

    created outlet for child components, navbar ans changed the app.js

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Title } from "react-head";
+
+export default function Homepage() {
+    return (
+        <>
+            <Title>ByteBloggers - Homepage</Title>
+            <h2 className="bg-red-500">Welcome to homepage</h2>
+        </>
+    );
+}
